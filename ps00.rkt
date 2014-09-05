bf055d5ac587a4418f75be6df808f9ad4f1d6448
kssk628 <kssk628@ccs.neu.edu>
2014-09-05 15:37:53 -0400

    Exercise problems solved

---
+++ ps00.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ps00) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Exercise question 1
+(* 366 24 60 60)
+(+ (* 365 24 60 60) (* 24 60 60))
